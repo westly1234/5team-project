8f9ab5247ee546bfcbb790ab3fffc08287d03b31
westly1234 <westly8931@naver.com>
2025-06-19 09:48:17 +0900

    전체 재추적 : media, db 포함

--- routine/urls.py
+++ routine/urls.py
@@ -1,27 +1,26 @@
+# routine/urls.py
+
 from django.urls import path
 from . import views
 
-app_name = 'routine' # URL을 템플릿에서 쉽게 부르기 위한 네임스페이스
+app_name = 'routine'
 
 urlpatterns = [
-    # http://.../routine/ -> routine_select_view 함수 실행
+    # 페이지 렌더링 URL
     path('', views.routine_select_view, name='select'),
-    
-    # http://.../routine/my-routines/ -> my_routines_view 함수 실행
     path('my-routines/', views.my_routines_view, name='my_routines'),
-    
-    # http://.../routine/gpt-plan/ -> gpt_plan_view 함수 실행
+    # 예: /routine/plan/5/ -> 특정 루틴의 상세/수정 페이지
+    path('plan/<int:routine_id>/', views.routine_plan_detail_view, name='routine_plan_detail'),
+
+    # 처리/액션 URL
     path('gpt-plan/', views.gpt_plan_view, name='gpt_plan'),
-    
-    # http://.../routine/custom-plan/ -> custom_plan_view 함수 실행
     path('custom-plan/', views.custom_plan_view, name='custom_plan'),
-    
-    # API 엔드포인트
-    # http://.../routine/api/exercises/ -> exercise_api 함수 실행
-    path('api/exercises/', views.exercise_api, name='api_exercises'),
-    # 예: /routine/edit/5/
+    # 예: /routine/edit/5/ -> 루틴 수정 내용 저장
     path('edit/<int:routine_id>/', views.edit_routine_view, name='edit_routine'),
-    # 예: /routine/delete/5/
+    # 예: /routine/delete/5/ -> 루틴 삭제
     path('delete/<int:routine_id>/', views.delete_routine_view, name='delete_routine'),
-    path('api/youtube-search/', views.youtube_search_api, name='api_youtube_search'), 
+    
+    # API URLs
+    path('api/exercises/', views.exercise_api, name='api_exercises'),
+    path('api/youtube-search/', views.youtube_search_api, name='api_youtube_search'),
 ]
