5698df4115b4f8fe6a303a0c9f070381a0c79944
westly1234 <westly8931@naver.com>
2025-07-02 17:32:28 +0900

    locales 업데이트

--- web/templatetags/custom_translate.py
+++ web/templatetags/custom_translate.py
@@ -1,5 +1,6 @@
 from django import template
 from web.utils import t as translate_text
+from django.utils.functional import lazy
 
 register = template.Library()
 
@@ -12,3 +13,6 @@ def t(key, **kwargs):
 @register.filter(name='t')
 def translate_filter(value):
     return translate_text(value)
+
+def t_lazy(key, **kwargs):
+    return lazy(lambda: translate_text(key, **kwargs), str)()
